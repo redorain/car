4861a2fcebcbc18ff9bc9ee01ffcd683315b063b
redorain <54568631+redorain@users.noreply.github.com>
2021-10-19 16:27:38 +0800

    Add files via upload

---
+++ redlight2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#encoding:utf-8
+
+time_out=5
+Infrared=20
+
+RPi.GPIO.setmode(RPi.GPIO.BCM)
+RPi.GPIO.setup(Infrared,RPi.GPIO.IN)
+
+try:
+    while True:
+	    if(RPi.GPIO.input(Infrared)==True):
+	    	print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+" Smoe is here !"
+	    else:
+	    	print time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))+" Nobody !"
+	    time.sleep(time_out)
+	    
+except keyboardInterrupt:
+    pass
+RPi.GPIO.cleanup()
+
