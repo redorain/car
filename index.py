b485a3e11526402d9a9f42a9e6b732e8e409e137
redorain <13991216410@163.com>
2021-11-06 10:33:09 +0800

    11.06

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from bottle import get,post,run,request,template
+from bottle import get, post, run, request, template
 
 import RPi.GPIO as GPIO
 import time
@@ -7,48 +7,53 @@ import random
 from run import *
 from csb import *
 from yuyin import *
+
 sys.setdefaultencoding('GB2312')
 
 state = 3
 Yuyin = 0
 
+
 def main(status):
+    global Yuyin
     if status == "yuyin":
-        #os.system('espeak -vzh "%s"' % "open")
-        print("open")
-        Yuyin = 1
-    if status == "yuyin":
-        #os.system('espeak -vzh "%s"' % "of")
-        print("close")
-        Yuyin = 0
+        if Yuyin == 0:
+            # os.system('espeak -vzh "%s"' % "open")
+            print("open")
+            Yuyin = 1
+        else:
+            # os.system('espeak -vzh "%s"' % "of")
+            print("close")
+            Yuyin = 0
     if status == "fly":
         state = 1
     if status == "remember":
         state = 2
     if status == "control":
         state = 3
-#    while Yuyin:
-#        yuyinwork()
+    #    while Yuyin:
+    #        yuyinwork()
     if status == "front":
         forward(1)
-#        stop(0.1)
+    #        stop(0.1)
     elif status == "leftFront":
         left(1)
-#        stop(0.1)
+    #        stop(0.1)
     elif status == "rightFront":
         right(1)
-#        stop(0.1)
+    #        stop(0.1)
     elif status == "rear":
         reverse(1)
-#        stop(0.1)
+    #        stop(0.1)
     elif status == "leftRear":
         left_0(1)
-#        stop(0.1)
+    #        stop(0.1)
     elif status == "rightRear":
         right_0(1)
-#        stop(0.1)
+    #        stop(0.1)
     elif status == "stop":
         stop(0.1)
+
 
 '''
 def avoid():
@@ -70,14 +75,18 @@ def avoid():
     time.sleep(2)
 '''
 
+
 @get("/")
 def index():
     return template("index")
+
+
 @post("/cmd")
 def cmd():
-    adss=request.body.read().decode()
-    print("press the button:"+adss)
+    adss = request.body.read().decode()
+    print("press the button:" + adss)
     main(adss)
     return "OK"
-run(host="0.0.0.0")
 
+
+run(host="0.0.0.0")
